70070f3df8caa1ddd84d95911b298e70d9e1a6b2
XenKos <158058743+XenKos@users.noreply.github.com>
2024-06-09 13:47:04 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,3 @@
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:kseni4ka78@localhost/rest_app")
---
+++ schemas.py
@@ -0,0 +1,24 @@
+
+from pydantic import BaseModel, EmailStr, Field
+from typing import Optional
+from datetime import date
+
+class ContactCreate(BaseModel):
+    first_name: str
+    last_name: str
+    email: EmailStr
+    phone_number: str
+    birthday: date
+    additional_info: Optional[str] = None
+
+class Contact(BaseModel):
+    id: int
+    first_name: str
+    last_name: str
+    email: EmailStr
+    phone_number: str
+    birthday: date
+    additional_info: Optional[str] = None
+
+    class Config:
+        orm_mode = True
